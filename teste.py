8452a37505fc542b149e2d0bd175ed7c311abcd5
Fabricio Vizzotto <fabricio.vizzotto@telessauders.ufrgs.br>
2018-06-13 08:57:23 -0300

    Finalização dos exercicios Python

--- teste.py
+++ teste.py
@@ -2,7 +2,7 @@ from problemsolver import ProblemSolver
 
 aplicacao = ProblemSolver()
 aplicacao.evenorodd(10, 1)
-aplicacao.faltaanos(fabricio,24)
+aplicacao.faltaanos("fabricio",24,1)
 aplicacao.takelist(20)
 aplicacao.comparelist()
 aplicacao.guessmynumba()
